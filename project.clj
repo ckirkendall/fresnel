65d4f5a9f2bf99fc3388ccb4db0ce3745d1d1a58
ckirkendall <ckirkendall@gmail.com>
2014-02-04 23:10:16 -0500

    added cljx to setup for clojurescript version

--- project.clj
+++ project.clj
@@ -3,4 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :profiles {:dev {:plugins [[com.keminglabs/cljx "0.3.2"]]
+                   :hooks [cljx.hooks]
+                   :cljx {:builds [{:source-paths ["src"]
+                                    :output-path "target/classes"
+                                    :rules :clj}
+                                   {:source-paths ["src"]
+                                    :output-path "target/classes"
+                                    :rules :cljs}]}}})
